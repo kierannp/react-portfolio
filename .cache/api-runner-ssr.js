39f8869c507f25a4e6aad72b34f5e8f24630f3c2
Kieran Nehil-Puleo <knp@Kierans-MacBook-Pro.local>
2021-02-20 15:14:12 -0500

    changed build command

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -3,7 +3,7 @@ var plugins = [{
       options: {"plugins":[]},
     },{
       plugin: require('/Users/knp/react-portfolio/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
-      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/gatsby-icon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":null},
+      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"./","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"./public/favicon-32x32.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"2eedf08f3472a74feaaa43c8bcb1c097"},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
 // var plugins = [
